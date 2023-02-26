6ec7538d3d57c1da1e1832cda1fc097e7f456a27
MO_HATEM <MO_HATEM@DESKTOP-247PBST>
2023-02-26 21:27:37 +0200

    "First Exercise in Assignment 3 (Arrays)"

---
+++ Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex1/Ex1.c
@@ -0,0 +1,20 @@
+/*
+ * Ex1.c
+ *
+ *  Created on: Feb 26, 2023
+ *      Author: MO_HATEM
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+
+	setbuf(stdout,NULL);
+
+	printf("Hello Array");
+
+	return 0;
+
+}
+
