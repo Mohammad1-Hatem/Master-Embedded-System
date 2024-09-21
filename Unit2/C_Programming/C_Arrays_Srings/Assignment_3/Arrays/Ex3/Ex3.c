bba2888bc4eca3982ef2321886fea99ca93723b1
Mo_7atem <Mo_7atem@DESKTOP-HSK5BM4>
2024-09-22 00:40:19 +0300

    22-9-2024

---
+++ Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex3/Ex3.c
@@ -0,0 +1,19 @@
+/*
+ * Ex3.c
+ *
+ *  Created on: ٢٢‏/٠٩‏/٢٠٢٤
+ *      Author: Mo_7atem
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+
+
+	for(int i=0; i<10;i++){
+		printf("i : %d\n",i+1);
+	}
+
+	return 0;
+}
