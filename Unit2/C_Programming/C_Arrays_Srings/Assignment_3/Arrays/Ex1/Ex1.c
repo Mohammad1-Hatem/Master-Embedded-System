0230fa53d5996151189093985fe0246422fd4be0
Mo_7atem <Mo_7atem@DESKTOP-HSK5BM4>
2024-09-30 02:37:11 +0300

    Ex1 30-9-2024

--- Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex1/Ex1.c
+++ Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex1/Ex1.c
@@ -1,8 +1,8 @@
 /*
  * Ex1.c
  *
- *  Created on: Feb 26, 2023
- *      Author: MO_HATEM
+ *  Created on: Sep 30, 2024
+ *      Author: Mo_7atem
  */
 
 
@@ -10,11 +10,42 @@
 
 int main(void){
 
+
+	float a1[2][2],a2[2][2],sum[2][2];
+
 	setbuf(stdout,NULL);
 
-	printf("Hello Array");
+	printf("Enter the element of the 1st element: \n");
+	for(int i=0; i<2;i++){
+		for(int j=0; j<2;j++){
+			printf("Enter a%d%d: ",i+1,j+1);
+			scanf(" %f",&a1[i][j]);
+		}
+	}
+
+
+	printf("\nEnter the element of the 2nd element: \n");
+	for(int i=0; i<2;i++){
+		for(int j=0; j<2;j++){
+			printf("Enter a%d%d: ",i+1,j+1);
+			scanf(" %f",&a2[i][j]);
+		}
+	}
+
+	for(int i=0; i<2;i++){
+		for(int j=0;j<2;j++){
+			sum[i][j]=a1[i][j]+a2[i][j];
+		}
+	}
+
+	printf("\n");
+	for(int i=0; i<2;i++){
+		for(int j=0; j<2;j++){
+			printf("%.2f\t", sum[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 
 }
-
