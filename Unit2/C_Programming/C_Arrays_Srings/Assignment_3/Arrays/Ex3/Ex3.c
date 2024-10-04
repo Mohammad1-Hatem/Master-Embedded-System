b505029a2ba91c06708ab094816fc9a8ea09a7df
Mo_7atem <Mo_7atem@DESKTOP-HSK5BM4>
2024-10-04 20:13:45 +0300

    Example 3

---
+++ Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex3/Ex3.c
@@ -0,0 +1,36 @@
+/*
+ * Ex3.c
+ *
+ *  Created on: Oct 4, 2024
+ *      Author: Mo_7atem
+ */
+
+#include <stdio.h>
+
+int main(void){
+
+	int n;
+	float num,sum=0;
+	float avg;
+
+	setbuf(stdout,NULL);
+
+	printf("Enter number of elements: ");
+	scanf("%d", &n);
+
+
+	int i=0;
+	while(i<n){
+		printf("Enter number: ");
+		scanf("%f",&num);
+		sum+=num;
+		i++;
+	}
+
+	avg=sum/n;
+	printf("The average: %f",avg);
+
+	return 0;
+
+}
+
