6b278b43e9cf351a83c150d63c0fdbc438d22c56
Mo_7atem <Mo_7atem@DESKTOP-HSK5BM4>
2024-10-04 23:37:21 +0300

    Ex3

--- Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex2/Ex2.c
+++ Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex2/Ex2.c
@@ -11,8 +11,28 @@
 
 int main(void){
 
-	printf("Hello World!");
+	int n;
+	float avg, num, sum=0;
+
+
+	setbuf(stdout,NULL);
+
+	printf("Enter number of elements: ");
+	scanf("%d", &n);
+
+
+	int i=0;
+	while(i<n){
+		printf("Enter number: ");
+		scanf("%f",&num);
+		sum+=num;
+		i++;
+	}
+
+	avg=sum/n;
+	printf("The average: %f",avg);
 
 	return 0;
 
 }
+
--- Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex3/Ex3.c
+++ Unit2/C_Programming/C_Arrays_Srings/Assignment_3/Arrays/Ex3/Ex3.c
@@ -9,28 +9,45 @@
 
 int main(void){
 
-	int n;
-	float avg, num, sum=0;
-
-
+	int rows,cols;
 	setbuf(stdout,NULL);
 
-	printf("Enter number of elements: ");
-	scanf("%d", &n);
+	printf("Enter number of rows: ");
+	scanf("%d",&rows);
 
+	printf("Enter number of columns: ");
+	scanf("%d",&cols);
 
-	int i=0;
-	while(i<n){
-		printf("Enter number: ");
-		scanf("%f",&num);
-		sum+=num;
-		i++;
+	int arr[rows][cols];
+
+	printf("\n");
+
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			printf("Enter element a%d%d: ",i+1,j+1);
+			scanf("%d",&arr[i][j]);
+		}
 	}
 
-	avg=sum/n;
-	printf("The average: %f",avg);
+	printf("\n");
+	printf("Entered Matrix: \n");
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			printf("%d   ",arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+
+	printf("Transposed Matrix: \n");
+	for(int j=0;j<cols;j++){
+		for(int i=0;i<rows;i++){
+			printf("%d   ",arr[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
-
 }
 
